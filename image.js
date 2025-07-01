8a23595acc6ed987564c185e90aab7cc10c74348
Anjali Prajapati <160333990+anjali67c@users.noreply.github.com>
2025-07-01 17:27:48 +0530

    query and image change

---
+++ image.js
@@ -0,0 +1,11 @@
+//to change images while hovering over the image
+
+function changeImage1() {
+let image = document.getElementById("image");
+image.src = "converse 2.webp";
+}
+
+function changeImage2() {
+    let image = document.getElementById("image");
+    image.src = "converse 3.webp";
+}
---
+++ query.js
@@ -0,0 +1,15 @@
+let tag = document.querySelector('h1')
+tag.style.color = 'red';
+tag.style.fontSize = '50px';
+
+let className = document.querySelector('.head2')
+className.style.color = 'blue';
+
+let id = document.querySelector('#head1');
+id.style.color = 'green';
+
+let list = document.querySelectorAll('li');
+list[0].style.color = 'purple';
+
+//we have unordered list have 10 li, even elemnt should have bg as yellow.
+//in counter we have even and odd numbers, the even numbers shuld have blue colour and odd numbers should have red colour. 
